c9b98f18fe639a0cd709db92fbea1d3804d6afe6
James <jcoulson95@gmail.com>
2016-10-09 20:59:38 +0100

    Started to add control functionality with a pushbutton

--- Iron_man_Led/Iron_man_Led.ino
+++ Iron_man_Led/Iron_man_Led.ino
@@ -1,8 +1,26 @@
+/*
+ * Iron Man NeoPixel Ring Control Sketch
+ * 
+ * This sketch is to beused with a 24 pixel neopxiel ring and used to display number of effects and patterns:
+ * -  The standard iron man whitish blue colour
+ * -  Rainbow effects 
+ * -  loading ring
+ * 
+ * The setup is fairly simple:
+ * -  the neopixel is wired to power with a 100uF electrolytic in parallel to protect it from surges
+ * -  TODO: there is a switch wired in series with the power supply
+ * -  the signal wire is connected to pin 3
+ * -  TODO: a pushbutton switch is wired to pin ___
+ * 
+ * 
+ */
+
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 3
+#define PIXEL_PIN 4
+#define BUTTON_PIN 3
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 uint8_t count = 0;
 uint8_t colour_index = 0;
@@ -11,12 +29,21 @@ int led_leader = 0;
 int position_lag = 0;
 int position_lag1 = 0;
 
+boolean button_state = false;
+#define BUTTON_DEBOUNCE_TIME 200
+#define BUTON_PRESS_TIME 1000
+uint8_t button_press_count = 0;
+unsigned long button_timer = 0;
+unsigned long button_press_timer = 0;
+
 #define BRIGHTNESS_FACTOR 10
+
 
 uint32_t iron_man = strip.Color(150,150,255);
 uint32_t off = strip.Color(0,0,0);
 
 void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   strip.begin();
   strip.show();
   strip.setBrightness(64);
@@ -29,8 +56,44 @@ void setup() {
 }
 
 void loop() {
-  randomPixelRing();
-  randomRing();
+  // update pixel ring
+  
+  // wait for specified delay time whilst checking state of button
+  // check if button pressed and if so take millis
+  if(!digitalRead(BUTTON_PIN) & button_state == false){
+    button_timer = millis();
+    button_state = true;
+  }
+  // check for button release
+  else if(digitalRead(BUTTON_PIN) & button_state == true){
+    button_timer = millis() - button_timer;
+    if(button_timer > BUTTON_DEBOUNCE_TIME){
+      if(button_press_count == 0){
+        button_press_count++;
+        button_press_timer = millis();
+      }
+      else{
+        button_press_timer = millis() - button_press_timer;
+        if(button_press_timer < BUTTON_PRESS_TIME){
+          // finish this off...
+        }
+      
+    }
+  }
+}
+
+void allLedsOff(void){
+  for(int n = 0; n<24; n++){
+    strip.setPixelColor(n, off);
+  }
+  strip.show();
+}
+
+void allLedsOn(uint32_t colour){
+    for(int n = 0; n<24; n++){
+      strip.setPixelColor(n, colour);
+    }
+    strip.show();
 }
 
 void randomRing(void){
@@ -84,7 +147,7 @@ void loading(){
 }
 
 void rainbow_wheel(){
-    for(int n=0; n<24; n++){
+  for(int n=0; n<24; n++){
     colour_index = count + (n*10);
     strip.setPixelColor(n, Wheel(colour_index));
   }
